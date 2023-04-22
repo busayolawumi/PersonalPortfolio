7518eb7a8ad74f72b4d87e49b0b1c86d4cf7a988
busayolawumi <busayoolawumi3@gmail.com>
2023-04-22 23:15:01 +0100

    Frontend: "Added new project"

--- src/Scripts/projectArray.js
+++ src/Scripts/projectArray.js
@@ -1,3 +1,4 @@
+import Quizapppic from '../assets/Quizapppic.png'
 import Skytrackerpic from '../assets/Skytrackerpic.png'
 import Datafinancepic from '../assets/Datafinancepic.png'
 import Twentyfortyeightpic from '../assets/Twentyfortyeightpic.png'
@@ -6,9 +7,17 @@ import Pingpongpic from '../assets/Pingpongpic.png'
 
 export const projectArray = [
     {
+        imgLink: Quizapppic,
+        name: "Quiz Dom",
+        description: "A quiz app website that is based on the famous Who Wants To Be A Millionaire game show",
+        languages: "•ReactJS • CSS ",
+        website: "https://quizdom.vercel.app/",
+        isHr: true
+    },
+    {
         imgLink: Skytrackerpic,
         name: "Sky Tracker",
-        description: "This global web site offers weather updates and climatological information for requested cities and countries supplied by Open Weather",
+        description: "This global website offers weather updates and climatological information for requested cities and countries supplied by Open Weather",
         languages: "•ReactJS • CSS • OpenWeather API",
         website: "https://skytracker.vercel.app/",
         isHr: true
@@ -16,17 +25,9 @@ export const projectArray = [
     {
         imgLink : Datafinancepic,
         name: "Data Finance",
-        description: "Landing page for a company that deals offers storage space and data analytics to other companies",
+        description: "Landing page for a company that offers storage space and data analytics to other companies",
         languages: "• ReactJS • TailwindCSS • JavaScript",
         website: "https://datafinanceapp.vercel.app/",
-        isHr: true
-    },
-    {
-        imgLink : Twentyfortyeightpic,
-        name: "2048",
-        description: "2048 is an easy and fun puzzle game. Even if you don't love numbers you will love this game. Try and beat your high score.",
-        languages: "• HTML • CSS • JavaScript",
-        website: "https://play2048.vercel.app/",
         isHr: true
     },
     {
@@ -38,11 +39,19 @@ export const projectArray = [
         isHr: true
     },
     {
-        imgLink : Pingpongpic,
-        name: "Ping Pong Scorer",
-        description: "It's a bit tricky when keeping count for a game of Ping Pong/Table Tennis, so this is a simple Ping Pong Score Keeper that does the job for you.",
-        languages: "• HTML • Bulma • JavaScript",
-        website: "https://ping-pong-counter.vercel.app/",
+        imgLink : Twentyfortyeightpic,
+        name: "2048",
+        description: "2048 is an easy and fun puzzle game. Even if you don't love numbers you will love this game. Try and beat your high score.",
+        languages: "• HTML • CSS • JavaScript",
+        website: "https://play2048.vercel.app/",
         isHr: false
-    }
+    },
+    // {
+    //     imgLink : Pingpongpic,
+    //     name: "Ping Pong Scorer",
+    //     description: "It's a bit tricky when keeping count for a game of Ping Pong/Table Tennis, so this is a simple Ping Pong Score Keeper that does the job for you.",
+    //     languages: "• HTML • Bulma • JavaScript",
+    //     website: "https://ping-pong-counter.vercel.app/",
+    //     isHr: false
+    // }
 ]
