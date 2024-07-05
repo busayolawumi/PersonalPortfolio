b90b60f09ba42b903147cdd80d1e92cbdbef8749
busayolawumi <busayoolawumi3@gmail.com>
2024-07-05 02:23:15 +0100

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import About from './sections/About'
-import Contact from './sections/Contact'
-import Footer from './sections/Footer'
-import Hero from './sections/Hero'
-import Navbar from './sections/Navbar'
-import Projects from './sections/Projects'
-import Skills from './sections/Skills'
+import React from "react";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
-  return (
-    <>
-      <Hero />
-      <Navbar />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </>
-  )
-}
+	return (
+		<>
+			<Hero />
+			<Navbar />
+			<About />
+			<Skills />
+			<Projects />
+			<Contact />
+			<Footer />
+			<SpeedInsights />
+		</>
+	);
+};
 
-export default App
+export default App;
