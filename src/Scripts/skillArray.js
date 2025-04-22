762f4f1f7f7a22011f23939c70c35ae6b4f1d179
busayolawumi <busayoolawumi3@gmail.com>
2025-04-22 09:52:55 +0100

    resume & skill update

--- src/Scripts/skillArray.js
+++ src/Scripts/skillArray.js
@@ -1,61 +1,59 @@
-import Javascripticon from '../assets/Jsicon.png'
-import Tailwindicon from '../assets/Tailwindicon.png'
-import Htmlicon from '../assets/Htmlicon.png'
-import Cssicon from '../assets/Cssicon.png'
-import Bootstrapicon from '../assets/Bootstrapicon.png'
-import Wordpressicon from '../assets/Wordpressicon.png'
-import Firebaseicon from '../assets/Firebaseicon.png'
-import Reacticon from '../assets/Reacticon.png'
-import Kotlinicon from '../assets/Kotlinicon.png'
-import Typescripticon from '../assets/Typescripticon.png'
-
+import Javascripticon from "../assets/Jsicon.png";
+import Tailwindicon from "../assets/Tailwindicon.png";
+import Htmlicon from "../assets/Htmlicon.png";
+import Cssicon from "../assets/Cssicon.png";
+import Bootstrapicon from "../assets/Bootstrapicon.png";
+import Wordpressicon from "../assets/Wordpressicon.png";
+import Firebaseicon from "../assets/Firebaseicon.png";
+import Reacticon from "../assets/Reacticon.png";
+import Kotlinicon from "../assets/Kotlinicon.png";
+import Typescripticon from "../assets/Typescripticon.png";
+import Nexticon from "../assets/Nexticon.png";
 
 export const skillArray = [
-    {
-        imgSrc: Reacticon,
-        altText: "React Icon",
-        skill: "REACTJS",
-    },
-    {
-        imgSrc: Typescripticon,
-        altText: "TypeScript Icon",
-        skill: "TYPESCRIPT",
-    },
-    {
-        imgSrc: Wordpressicon,
-        altText: "Wordpress Icon",
-        skill: "WORDPRESS",
-    },
-    {
-        imgSrc: Firebaseicon,
-        altText: "Firebase Icon",
-        skill: "FIREBASE",
-    },
-    
-    {
-        imgSrc: Htmlicon,
-        altText: "HTML Icon",
-        skill: "HTML",
-    },
-    {
-        imgSrc: Cssicon,
-        altText: "CSS Icon",
-        skill: "CSS",
-    },
-    {
-        imgSrc: Javascripticon,
-        altText: "Javascript Icon",
-        skill: "JAVASCRIPT",
-    },
-    {
-        imgSrc: Tailwindicon,
-        altText: "Tailwind Icon",
-        skill: "TAILWIND",
-    },
-    {
-        imgSrc: Bootstrapicon,
-        altText: "BootStrap Icon",
-        skill: "BOOTSTRAP",
-    },
-] 
-                     
+	{
+		imgSrc: Nexticon,
+		altText: "Next Icon",
+		skill: "NEXT.JS",
+	},
+	{
+		imgSrc: Reacticon,
+		altText: "React Icon",
+		skill: "REACTJS",
+	},
+	{
+		imgSrc: Typescripticon,
+		altText: "TypeScript Icon",
+		skill: "TYPESCRIPT",
+	},
+	{
+		imgSrc: Javascripticon,
+		altText: "Javascript Icon",
+		skill: "JAVASCRIPT",
+	},
+	{
+		imgSrc: Wordpressicon,
+		altText: "Wordpress Icon",
+		skill: "WORDPRESS",
+	},
+	{
+		imgSrc: Htmlicon,
+		altText: "HTML Icon",
+		skill: "HTML",
+	},
+	{
+		imgSrc: Cssicon,
+		altText: "CSS Icon",
+		skill: "CSS",
+	},
+	{
+		imgSrc: Tailwindicon,
+		altText: "Tailwind Icon",
+		skill: "TAILWIND",
+	},
+	{
+		imgSrc: Bootstrapicon,
+		altText: "BootStrap Icon",
+		skill: "BOOTSTRAP",
+	},
+];
